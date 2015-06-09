b0c8f5029c4b22804863602b0f4c85510e28a390
Jonathan Hartman <j@p4nt5.com>
2015-06-08 18:16:49 -0700

    Initial commit; freshly generated

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Plex Home Theater Cookbook CHANGELOG
+====================================
+
+v0.0.1 (2015-06-08)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'plex-home-theater'
+maintainer       'Jonathan Hartman'
+maintainer_email 'j@p4nt5.com'
+license          'apache2'
+description      'Installs/Configures plex-home-theater'
+long_description 'Installs/Configures plex-home-theater'
+version          '0.0.1'
+# rubocop:enable SingleSpaceBeforeFirstArg
